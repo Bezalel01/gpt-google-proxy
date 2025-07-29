e9393f818d0202067272e4ce505fb423c30414bd
Bezalel01 <bezaleluser@gmail.com>
2025-07-29 03:57:07 +0300

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -6,22 +6,21 @@ export default async function handler(req, res) {
   }
 
   try {
-    console.log("Received body:", req.body);
-
     const response = await fetch("https://script.google.com/macros/s/AKfycbw0wMiIlJiOZjBIdZzn-qSUI7V6C7Pqqum2Ziv-oZ15QsonRroOwL-MHejeZSlY_3XRcg/exec", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
       body: JSON.stringify(req.body),
     });
 
     const text = await response.text();
-    console.log("Google Script response:", text);
-
     let data;
+
     try {
       data = JSON.parse(text);
     } catch {
-      data = { status: "success", raw: text }; // fallback
+      data = { status: "success", raw: text };
     }
 
     return res.status(200).json(data);
